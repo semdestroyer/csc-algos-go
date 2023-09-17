ee752c305441622728a41c01c53f1feaab4f9f4d
Arsenii <semdestroyer@yandex.ru>
2023-09-18 01:44:26 +0300

    solved first two tasks

---
+++ fibonacci/2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		return
+	}
+	fmt.Println(fibbonaci(n))
+}
+
+func fibbonaci(n int) int {
+	arr := make([]int, n+1)
+	arr[0] = 1
+	arr[1] = 1
+	if n <= 2 {
+		return arr[n-1]
+	}
+	for i := 2; i < n; i++ {
+		arr[i] = (arr[i-1] + arr[i-2]) % 10
+	}
+
+	return arr[n-1]
+}
---
+++ go.mod
@@ -0,0 +1,1 @@
+module csc_algos_stepik
